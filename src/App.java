ad215d5d8b2752beb273a368c86111bca0bfacdc
NathanTCY <nathan4168@gmail.com>
2021-07-27 22:30:39 +0800

    Nathan changed this again

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Good Morning");
-        
+        System.out.println("Nathan printed this 3rd line");
         generate[] genny = new generate[2];
         genny[0] = new generate();
         genny[1] = new generate();
