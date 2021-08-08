7337bcfd9f1db9262bb95c82960a5484191ce5c1
ShiHan <tsh057net@gmail.com>
2021-08-08 16:45:40 +0800

    prototype for table view

---
+++ src/Controller/BuyerTable.java
@@ -0,0 +1,19 @@
+package Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class BuyerTable implements Initializable{
+
+    @FXML
+    private AnchorPane Anchor;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+}
--- src/SQL/createAccountTable.sql
+++ src/SQL/createAccountTable.sql
@@ -1,6 +1,7 @@
 DROP TABLE fCe5HJjPF6.Account;
 CREATE TABLE Account (
     accountID     varchar(6)  not null,
+    userName   varchar(15) not null,
     password      varchar(40)  not null,
     email         varchar(250),
     mobileNo      varchar(20)   not null,
@@ -8,28 +9,28 @@ CREATE TABLE Account (
     primary key(accountID)
 );
 
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5450', 'EU3ltALd', 'lbissett0@cisco.com', '019-4913377', '17/04/2013');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A6483', 'eASicxHZ8pT', 'afarragher1@networksolutions.com', '010-23418977', '11/03/2014');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5331', '4WqQZH6bjf9', 'htutchings2@mtv.com', '010-91149557', '18/09/2017');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A4464', 'yZiEMvyd6', 'ycranna3@rediff.com', '019-1699777', '04/10/2019');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A3524', 'DaQMAPWf', 'brhoades4@webs.com', '019-3093328', '26/08/2020');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5528', 'ySdT4Vx', 'kocannovane5@microsoft.com', '017-3798746', '23/06/2017');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A9682', 'Mehf7IqdE', 'jalecock6@sphinn.com', '019-3700727', '09/04/2017');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A8763', 'niA0t56', 'wramel7@hao123.com', '011-31629718', '22/03/2018');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A4468', 'uxZXT2nqM', 'jhawgood8@furl.net', '019-2149346', '03/11/2020');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5417', '2ZXuti', 'akaemena9@php.net', '016-2978084', '25/12/2011');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A6955', 'nmuybeVe7', 'abridgwooda@si.edu', '012-0337201', '07/02/2017');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5012', 'HWDJx43i0', 'acharlestonb@meetup.com', '010-89763702', '28/11/2016');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A3134', 'jRyfTCnlGX7', 'fperlec@ow.ly', '019-8350861', '11/09/2020');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1632', 'cNVEfBHHMUrS', 'vseebrightd@youku.com', '010-50833056', '02/12/2019');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A7076', 'UzOCK7HkvY', 'jjadczake@unc.edu', '019-6377185', '06/01/2014');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1215', 'vZic35NV', 'sleyburnf@yolasite.com', '018-4363019', '10/02/2013');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A8041', 'GHvJjpQP5CPi', 'miversg@ihg.com', '018-9342397', '21/04/2015');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A7209', 'bVgmGG7Jy', 'rcholoninh@sakura.ne.jp', '017-3005951', '15/12/2012');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1071', 'myGDOXI1vStO', 'gjosei@foxnews.com', '014-2839685', '31/07/2012');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1248', 'wOH0LF', 'afalshawj@youtube.com', '011-47296220', '01/12/2019');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A8296', 'BltEnfu9547Q', 'aborthek@admin.ch', '019-8553233', '10/07/2012');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A9347', 'YlWehm', 'rgrocuttl@alibaba.com', '019-0980094', '07/01/2016');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A5363', 'tR9ISN', 'kmckintoshm@hao123.com', '013-1925560', '25/06/2012');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1321', 'UxDwcBHjrYg', 'jmounsien@scientificamerican.com', '010-92783768', '21/11/2018');
-insert into fCe5HJjPF6.Account (accountID, password, email, mobileNo, regiDate) values ('A1930', 'Me0j7p', 'hczaplao@360.cn', '010-87707061', '15/01/2015');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5450', 'jbernardon0', 'tByDG4pt', 'tbrimmicombe0@wp.com', '011-82977551', '16/02/2021');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A6483', 'cdodd1', 'cUk9mlry', 'gdibson1@creativecommons.org', '019-9835435', '18/04/2011');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5331', 'tbaudin2', '1MaKIRxui', 'cflori2@squidoo.com', '011-26956246', '27/05/2014');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A4464', 'lgillis3', 'zIWUeBI80ic7', 'bfenton3@cpanel.net', '011-29558244', '31/10/2016');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A3524', 'wbraunle4', 'q21XCFu7', 'bwitchell4@flickr.com', '019-7598774', '02/03/2018');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5528', 'tclubbe5', 'bJL21ekl', 'sdaile5@bloomberg.com', '011-95495106', '05/07/2019');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A9682', 'dwildbore6', 'HRVez4T2', 'bcharte6@nyu.edu', '011-19169824', '06/02/2017');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A8763', 'msavill7', 'U88QiFJ7', 'pkingzett7@linkedin.com', '019-1892498', '02/12/2013');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A4468', 'dyancey8', 'OE5SryltavP', 'jsiddens8@google.com.au', '010-48685412', '24/02/2011');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5417', 'apetrenko9', '9OADeKa', 'ochaudrelle9@chron.com', '010-20173210', '23/08/2020');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A6955', 'ewoodalla', 'yv1to9', 'qroystea@baidu.com', '010-75608536', '09/12/2015');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5012', 'rsandyfirthb', 'V4dAvf', 'skienlb@constantcontact.com', '017-1463014', '23/08/2019');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A3134', 'salenichicovc', '78xqOXI', 'bslottc@state.gov', '019-4063011', '20/07/2019');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1632', 'edearled', 'wuDNnF1f5y', 'idakind@csmonitor.com', '019-2380697', '13/03/2015');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A7076', 'mluce', 'esWIkiAS', 'hshetliffe@ning.com', '018-5397459', '10/03/2018');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1215', 'eisaksonf', 'sUqad4h', 'rchudleighf@odnoklassniki.ru', '018-0058845', '25/05/2011');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A8041', 'zstoppsg', 'uCaVu3ZU', 'clumbleyg@gmpg.org', '011-43407433', '24/01/2013');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A7209', 'emolesworthh', 'a9iatKZ', 'tpartenerh@cnbc.com', '014-1863032', '16/04/2013');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1071', 'rshulveri', '2kaz606', 'bswiersi@jalbum.net', '017-4308375', '21/02/2013');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1248', 'ccrichtonj', 'pHyMgq8Q', 'viacovinij@aboutads.info', '017-6368574', '18/10/2017');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A8296', 'mezzyk', 'DwWyzxlHIVK', 'jvedyasovk@mapquest.com', '019-2818757', '18/10/2012');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A9347', 'hfidgettl', 'gRgYBiPd', 'azisnerosl@livejournal.com', '019-5933317', '21/12/2010');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A5363', 'aberthelm', 'DgWXBt0qd4', 'dpettingerm@yelp.com', '010-30143929', '11/07/2017');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1321', 'hmuckloen', 'cn4IjKiJ2OEx', 'fgiottinin@independent.co.uk', '016-8371211', '28/06/2016');
+insert into fCe5HJjPF6.Account (accountID, userName, password, email, mobileNo, regiDate) values ('A1930', 'vgillbankso', '2e12ct5OBT', 'kgloyo@ftc.gov', '010-84390783', '14/05/2014');
