077f86c13e9ba4ba295d4d7dc3274a721684503f
yeechooili <yeechooili1120@gmail.com>
2021-09-12 16:32:18 +0800

    controller:CheckoutPayment little alter

--- src/Controller/Popup/CheckoutPayment.java
+++ src/Controller/Popup/CheckoutPayment.java
@@ -54,7 +54,7 @@ public class CheckoutPayment implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
-        lblTotal.setText(String.format("%.2f",data.getCart().calcTotalAmount()));
+        lblTotal.setText(String.format("%.2f",data.getBuyer().getCart().calcTotalAmount()));
         ObservableList<String> options = FXCollections.observableArrayList(banks.keySet());
         dropdownBank.getItems().addAll(options);        
     }       
