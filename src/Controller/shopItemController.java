978aeb481e045cdb8d986c895b3a2cf16f56b4ae
ShiHan <tsh057net@gmail.com>
2021-09-12 14:29:01 +0800

    debug

--- src/Controller/shopItemController.java
+++ src/Controller/shopItemController.java
@@ -1,10 +1,8 @@
 package Controller;
 import java.util.ArrayList;
-import java.util.List;
 
 import Classes.Shop;
 import ListenerInterfaces.BuyerHomeListeners;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,6 +17,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class ShopItemController{
+    
     @FXML private HBox shopItem;
     @FXML private VBox shopImgContainer;
     @FXML private ImageView shopImg;
